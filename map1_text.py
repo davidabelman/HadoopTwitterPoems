f9fa862c1c295d416a2b8f567d523900bba89196
David <davidabelman@gmail.com>
2014-11-23 11:26:32 +0700

    Updated to pass through a blank ID

--- map1_text.py
+++ map1_text.py
@@ -4,7 +4,7 @@
 # Input: text data
 # Extracts sentences, makes up other info
 # Filters for conditions --> no RT, contains certain hashtag/word(s), certain length, remove hashtags... etc.
-# Output: (text, date, user)
+# Output: (text, date, user, id)
 
 import sys
 import json
@@ -17,10 +17,11 @@ for line in sys.stdin:
 		# Extract info
 		tweet_time = 'N/A'
 		tweet_text = line.decode('utf-8').strip()
-		tweet_user = 'N/A'							
+		tweet_user = 'N/A'					
+		tweet_id = 'N/A'							
 
 		# Clean tweet and filter, output to STDOUT
 		tweet_text = tweet_text_filter(tweet_text)
 		if tweet_text:
 			# write the results to STDOUT (standard output), tab-delimited;
-			print '%s\t%s\t%s' % (tweet_text, tweet_time, tweet_user)
+			print '%s\t%s\t%s' % (tweet_text, tweet_time, tweet_user, tweet_id)
